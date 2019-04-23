45281d6ad53b467a30d3f0bbe6a205351a5c3868
swelham <stuart.welham@gmail.com>
2019-04-23 11:09:16 +0100

    fixed broken code spec

--- lib/faktory_worker/worker_logger.ex
+++ lib/faktory_worker/worker_logger.ex
@@ -16,19 +16,18 @@ defmodule FaktoryWorker.WorkerLogger do
   def log_beat(:ok, _, wid), do: log_info("Heartbeat Succeeded", wid)
   def log_beat(:error, _, wid), do: log_info("Heartbeat Failed", wid)
 
+  @spec log_fetch(:error, wid :: String.t(), error :: String.t()) :: :ok | {:error, any()}
+  def log_fetch(:error, wid, error) do
+    log_info("Failed to fetch job due to '#{error}'", wid)
+  end
+
   @spec log_failed_ack(:ok | :error, jid :: String.t(), args :: any()) :: :ok | {:error, any()}
   def log_failed_ack(:ok, jid, args) do
     log_info("Error sending 'ACK' acknowledgement to faktory", jid, args)
   end
 
-  @spec log_fetch(:error, jid :: String.t(), args :: any()) :: :ok | {:error, any()}
   def log_failed_ack(:error, jid, args) do
     log_info("Error sending 'FAIL' acknowledgement to faktory", jid, args)
-  end
-
-  @spec log_fetch(:error, wid :: String.t(), error :: String.t()) :: :ok | {:error, any()}
-  def log_fetch(:error, wid, error) do
-    log_info("Failed to fetch job due to '#{error}'", wid)
   end
 
   defp log_info(message) do
