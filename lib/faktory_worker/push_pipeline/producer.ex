1028eabb4bee3d6bc086a5a01ff23ec4df7276ad
swelham <stuart.welham@gmail.com>
2019-04-23 10:11:57 +0100

    default producer buffer_size and support configuring it

--- lib/faktory_worker/push_pipeline/producer.ex
+++ lib/faktory_worker/push_pipeline/producer.ex
@@ -3,10 +3,14 @@ defmodule FaktoryWorker.PushPipeline.Producer do
 
   use GenStage
 
-  def init(_opts) do
-    {:producer, %{jobs: []}}
+  @impl true
+  def init(opts) do
+    buffer_size = Keyword.get(opts, :buffer_size, :infinity)
+
+    {:producer, %{jobs: []}, buffer_size: buffer_size}
   end
 
+  @impl true
   def handle_demand(demand, %{jobs: jobs} = state) do
     {dispatch_jobs, remaing_jobs} = Enum.split(jobs, demand)
 
