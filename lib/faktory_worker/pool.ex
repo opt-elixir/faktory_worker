ff61c7e2b9148f9f4d4dc4e3da88bcaa1657b909
John Griffin <john@griffin.io>
2019-05-02 07:38:26 -0400

    debug logging

--- lib/faktory_worker/pool.ex
+++ lib/faktory_worker/pool.ex
@@ -1,21 +1,26 @@
 defmodule FaktoryWorker.Pool do
   @moduledoc false
 
+  require Logger
+
   def child_spec(opts) do
     name = opts[:name]
     pool_name = format_pool_name(name)
     pool_config = Keyword.get(opts, :pool, [])
 
-    :poolboy.child_spec(
-      pool_name,
-      [
-        {:name, {:local, pool_name}},
-        {:worker_module, FaktoryWorker.ConnectionManager.Server},
-        {:size, Keyword.get(pool_config, :size, 10)},
-        {:max_overflow, Keyword.get(pool_config, :size, 10)}
-      ],
-      Keyword.get(opts, :connection, [])
-    )
+    spec =
+      :poolboy.child_spec(
+        pool_name,
+        [
+          {:name, {:local, pool_name}},
+          {:worker_module, FaktoryWorker.ConnectionManager.Server},
+          {:size, Keyword.get(pool_config, :size, 10)},
+          {:max_overflow, Keyword.get(pool_config, :size, 10)}
+        ],
+        Keyword.get(opts, :connection, [])
+      )
+
+    spec
   end
 
   @spec format_pool_name(name :: atom()) :: atom()
