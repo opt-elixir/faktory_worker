018d4bc668421bb01240a18223c42b8a24d7f7b3
Chase Gilliam <chase.gilliam@gmail.com>
2024-12-19 12:12:29 -0500

    correct version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FaktoryWorker.MixProject do
   def project do
     [
       app: :faktory_worker,
-      version: "1.9.9",
+      version: "1.9.8",
       elixir: "~> 1.8",
       description: description(),
       package: package(),
